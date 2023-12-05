4e1c95f44371fbc261704ff8d814ec6262e49ee7
Mark Glasman <glasman.mark@gmail.com>
2023-12-05 14:42:02 -0500

    made it so I can successfully render my main.jsx

--- index.cjs
+++ index.cjs
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+
 const client = require('./db/client.cjs');
 client.connect();
 
+app.use('/assets', express.static(__dirname + '/dist/assets'))
+
 app.get('/', (req, res, next) => {
-  res.send("What's cracking, world?")
+  res.type('text/html');
+  res.sendFile(__dirname + '/dist/index.html')
 })
 
-const PORT = process.env.PORT || 8080;
+
+
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  console.log('app render')
   return (
     <>
-    
+    <h1>a code project a day keeps the therapist employed</h1>
     </>
   )
 }
