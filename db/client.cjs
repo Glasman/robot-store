1d51651cb545320ab0967b51b3718dc8d3d0d882
Mark Glasman <glasman.mark@gmail.com>
2023-12-05 15:05:55 -0500

    added code to hopefully make deploy work

--- db/client.cjs
+++ db/client.cjs
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/robotstore');
+const connection = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/robotstore');
+const client = new Client(connection);
 
 module.exports = client;
