c6a5081df9058c216d0bbeaf4a67e23129087ead
Mark Glasman <glasman.mark@gmail.com>
2023-12-05 11:31:21 -0500

    connected server to db

--- index.cjs
+++ index.cjs
@@ -1,2 +1,12 @@
 const express = require('express');
+const app = express();
 
+const client = require('./db/client.cjs');
+client.connect();
+
+app.get('/', (req, res, next) => {
+  res.send("What's cracking, world?")
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
